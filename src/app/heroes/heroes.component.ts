abb76cb0ce5c116c4464e403b5fa032455ddd05c
Pushkar <Anjali.Pushkar@ceridian.com>
2022-08-20 01:21:03 +0530

    Observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +11,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService :HeroService) { }
 
-  heroes= HEROES;
+  heroes: Hero[] = [];
   
   selectedHero?: Hero;
   onSelect(hero:Hero): void{
     this.selectedHero = hero;
   }
 
+  getHeroes(): void{
+    this.heroService.getHeroes().
+    subscribe(heroes=> this.heroes= heroes);
+  }
+
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
